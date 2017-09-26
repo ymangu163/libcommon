92ce7a3b35125c1079cbea12ab40afa36a984d3e
ymangu <shiping@acnet.co>
2017-09-26 16:17:02 +0800

    添加displayUtil

--- src/main/java/com/common/utils/DisplayUtil.java
+++ src/main/java/com/common/utils/DisplayUtil.java
@@ -1,8 +1,53 @@
 package com.common.utils;
 
+import android.app.Activity;
+import android.content.Context;
+import android.util.DisplayMetrics;
+
 /**
  * Created by gao on 2017/9/26.
  */
 
 public class DisplayUtil {
+
+    /**
+     * 根据手机的分辨率从 dip 的单位 转成为 px(像素)
+     */
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp==dip
+     */
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * get screen height of this cellphone
+     * @param context
+     * @return
+     */
+    public static int getMobileHeight(Context context) {
+        DisplayMetrics dm = new DisplayMetrics();
+        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int height = dm.heightPixels;  //得到高度
+        return height;
+    }
+    /**
+     * get screen width of this cellphone
+     * @param context
+     * @return
+     */
+    public static int getMobileWidth(Context context) {
+        DisplayMetrics dm = new DisplayMetrics();
+        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;    //得到宽度
+        return width;
+    }
+
+
 }
